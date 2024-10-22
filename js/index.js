1d59780b2518b3fee789ae578baa32bf2b6e1345
kim jeong ho <zzx5678@naver.com>
2024-10-22 14:11:44 +0900

    수정

--- js/index.js
+++ js/index.js
@@ -14,6 +14,10 @@ function loadContent(url) {
             if (url === 'html/exFinder/ex_2.html') {
                 initializeSlide();
             }
+            // 'ex_3.html'이 로드되었을 때만 
+            if (url === 'html/exFinder/ex_3.html') {
+                exFinder_sub_load('html/exFinder/sub/sub_1.html')
+            }
         })
         .catch(error => {
             console.error('페이지 로드 실패:', error);
@@ -21,10 +25,21 @@ function loadContent(url) {
         });
 }
 
-// 페이지가 로드되면 기본 화면 로드
-/*
-window.addEventListener('load', function () {
-    console.log("기본 페이지를 로드합니다.");
-    loadContent('html/first_page.html');
-});
-*/
+function exFinder_sub_load(url) {
+    const content_wrapper = document.getElementById('exFinder_sub_load');
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('네트워크 오류 발생: ' + response.statusText);
+            }
+            return response.text();
+        })
+        .then(data => {
+            content_wrapper.innerHTML = data;  // 로드된 HTML 내용을 content_wrapper에 삽입
+
+        })
+        .catch(error => {
+            console.error('페이지 로드 실패:', error);
+            content_wrapper.innerHTML = '<p>페이지 로드에 실패했습니다. 다시 시도해주세요.</p>';
+        });
+}
